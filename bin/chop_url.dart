94f5bc151b8ba9055c0fe4982675181982e3a8a5
Evanderson Vasconcelos <evandersonvasconcelos23@gmail.com>
2025-05-06 12:45:56 -0300

    update port

--- bin/chop_url.dart
+++ bin/chop_url.dart
@@ -1,14 +1,18 @@
+import 'dart:io';
+
 import 'package:chop_url/database/db.dart';
 import 'package:chop_url/router.dart';
 import 'package:darto/darto.dart';
 
 void main() async {
   await initDb();
+
+  final port = int.tryParse(Platform.environment['PORT'] ?? '8080') ?? 8080;
   final app = Darto();
 
   app.use(appRouter());
 
-  app.listen(3000, () {
-    print('Server running on port 3000');
+  app.listen(port, () {
+    print('Server running on port $port');
   });
 }
